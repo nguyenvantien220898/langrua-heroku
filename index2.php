e934501240b500e9562c20eaec133c6d86a9deb4
Tien Nguyen <tienrua066@gmail.com>
2019-05-21 19:07:03 +0700

    111

---
+++ index2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Việt Nam</title>
+</head>
+<body>
+
+Cú pháp của php phải đặt trong
+<pre>
+    <?php //code php trong chạy script ?>
+    Lệnh echo sẽ là xuất ra màn hình .
+Mỗi lệnh php thì phải kết thúc bằng ;
+Khi echo 1 chuỗi phải để chuỗi đó trong "" hoặc ''
+Nếu echo 1 số thì không cần để trong "" hoặc ''
+</pre>
+
+<pre>
+    <?php
+    echo "index.php";
+    echo "<br/>";
+    $vietnam = 'dân chủ cộng hòa';
+    //khai báo biến bên dưới
+    echo $vietnam;
+    echo "<br/>";
+    //define(tenbien,value)
+    define('name','nguyen tien rua');
+    echo name;
+    echo "<br/>";
+
+    echo 5;
+    echo "<p>php in ra thẻ html</p>";
+
+    ?></pre>
+</body>
+</html>
